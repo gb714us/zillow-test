a9217533fb9c914e12a961df552a91f292bd8cec
gb714us <gb714us@gmail.com>
2017-03-19 14:44:54 -0700

    added more infrastructure

--- server/node/app.js
+++ server/node/app.js
@@ -1,1 +1,19 @@
 const express = require('express');
+//Routes
+const searchRouter = require("./routes/search.js");
+const app = express();
+
+
+//default + middleware
+app.disable('X-Powered-By');
+app.use(express.static('../../public'));
+app.use('/search', searchRouter);
+
+
+
+
+app.get('/', (req, res) => res.send('Hello World'));
+
+app.listen(8080, function(){
+    console.log("Running now on port..." + this.address().port);
+});
